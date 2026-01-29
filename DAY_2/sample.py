c07884bfd6107d6812c810c00ba28d5d22a4af3d
chaitanya krishna <chaitanyakrishna294@gmail.com>
2026-01-30 01:13:42 +0530

    day_2

--- DAY_2/sample.py
+++ DAY_2/sample.py
@@ -6,10 +6,10 @@ X = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]).reshape(-1, 1)
 #Y is Scores
 Y = np.array([15, 25, 35, 48, 55, 68, 72, 85, 90, 96])
 
-model=LinearRegression()
-model.fit(X,Y)
+model=LinearRegression() #select model
+model.fit(X,Y) #train model
 Hours=int(input("Enter the no of Hours He/She Studied:"))
-predict=model.predict([[Hours]])
+predict=model.predict([[Hours]]) #predict 
 print(f"Marks: {predict[0]:.2f}")
 
 plt.scatter(X,Y,color='cyan',label="Actual Scores")
